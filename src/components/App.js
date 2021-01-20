a2a667882442b85d1c4de170a7554fa8e4df8632
jimanjang <67411280+jimanjang@users.noreply.github.com>
2021-01-20 14:29:15 +0900

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React , { useState } from "react";
+import AppRouter from "components/Router";
+import {authService} from "fbase";
+
+function App() {
+  console.log(authService.currentUser);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return <>
+    <AppRouter isLoggedIn={isLoggedIn} />
+    <footer>&copy; Nwitter {new Date().getFullYear()} </footer>
+  </>;
+}
+
+export default App;
