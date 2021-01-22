2b05ca5fb711da4d72626a80c98403f263210b0e
jimanjang <67411280+jimanjang@users.noreply.github.com>
2021-01-22 22:07:14 +0900

    Add files via upload

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { dbService } from "fbase";
 
 const Home = () => {
     const [jweet, setJweet] = useState("");
+    const [jweets, setJweets] = useState([]);
+    const getJweets = async () => {
+        const dbJweets = await dbService.collection("jweets").get();
+        dbJweets.forEach((document) => {
+            const jweetObject = {
+                ...document.data(),
+                id:document.id,
+            }
+            setJweets(prev => [document.data(), ...prev])
+        });
+    };
+    useEffect(() => {
+        getJweets();
+    }, []);
     const onSubmit = async (event) => {
         event.preventDefault();
         await dbService.collection("jweets").add({
@@ -17,6 +31,7 @@ const Home = () => {
         } = event;
         setJweet(value);
     };
+    console.log(jweets);
     return (
     <div>
         <form onSubmit={onSubmit}>
@@ -29,6 +44,13 @@ const Home = () => {
             />
             <input type="submit" value="Jweet" />
         </form>
+        <div>
+            {jweets.map((jweet) => (
+                <div key={jweet.id}>
+                    <h4>{jweet.jweet}</h4>
+                </div>
+        ))}
+        </div>
     </div>
     );
 };
