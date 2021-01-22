b68b33a903a69d3dcd07eea8ebf43b169e9b4e23
jimanjang <67411280+jimanjang@users.noreply.github.com>
2021-01-22 21:22:51 +0900

    Add files via upload

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
+import { dbService } from "fbase";
 
 const Home = () => {
     const [jweet, setJweet] = useState("");
-    const onSubmit = (event) => {
+    const onSubmit = async (event) => {
         event.preventDefault();
+        await dbService.collection("jweets").add({
+            jweet,
+            createdAt: Date.now(),
+        });
+        setJweet("");
     };
     const onChange = (event) => {
         const { 
@@ -15,12 +21,13 @@ const Home = () => {
     <div>
         <form onSubmit={onSubmit}>
             <input 
-            value={jweet} 
-            onChange={onChange} 
-            type="text" 
-            placeholder="What's on your mind?" 
-            maxLength={120} />
-            <input type="submit" value="Jtweet" />
+                value={jweet} 
+                onChange={onChange} 
+                type="text" 
+                placeholder="What's on your mind?" 
+                maxLength={120} 
+            />
+            <input type="submit" value="Jweet" />
         </form>
     </div>
     );
