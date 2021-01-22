9dfadddbeb463e6558c7fbd6b39f66b85b7750e8
jimanjang <67411280+jimanjang@users.noreply.github.com>
2021-01-22 20:49:25 +0900

    Add files via upload

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+    const [jweet, setJweet] = useState("");
+    const onSubmit = (event) => {
+        event.preventDefault();
+    };
+    const onChange = (event) => {
+        const { 
+            target:{value}, 
+        } = event;
+        setJweet(value);
+    };
+    return (
+    <div>
+        <form onSubmit={onSubmit}>
+            <input 
+            value={jweet} 
+            onChange={onChange} 
+            type="text" 
+            placeholder="What's on your mind?" 
+            maxLength={120} />
+            <input type="submit" value="Jtweet" />
+        </form>
+    </div>
+    );
+};
 export default Home;
