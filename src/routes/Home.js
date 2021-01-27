7bbb5fb2d948830d0ce3bcb5e1a481632b085ef2
jimanjang <67411280+jimanjang@users.noreply.github.com>
2021-01-27 22:11:37 +0900

    Add files via upload

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { dbService } from "fbase";
+import { v4 as uuidv4 } from "uuid";
+import { dbService, storageService } from "fbase";
 import Jweet from "components/Jweet";
 
 const Home = ({ userObj }) => {
     const [jweet, setJweet] = useState("");
     const [jweets, setJweets] = useState([]);
+    const [attachment, setAttachment] = useState();
     useEffect(() => {
         dbService.collection("jweets").onSnapshot(snapshot => {
             const jweetArray = snapshot.docs.map(doc => ({
@@ -16,12 +18,15 @@ const Home = ({ userObj }) => {
     }, []);
     const onSubmit = async (event) => {
         event.preventDefault();
-        await dbService.collection("jweets").add({
+        const fileRef = storageService.ref().child(`${userObj.uid}/${uuidv4()}`);
+        const response = await fileRef.putString(attachment, "data_url");
+        console.log(response);
+        /*await dbService.collection("jweets").add({
             text : jweet,
             createdAt: Date.now(),
             creatorId: userObj.uid,
         });
-        setJweet("");
+        setJweet("");*/
     };
     const onChange = (event) => {
         const { 
@@ -29,6 +34,21 @@ const Home = ({ userObj }) => {
         } = event;
         setJweet(value);
     };
+    const onFileChange = (event) => {
+        const {
+            target:{files},
+        } = event;
+        const theFile = files[0];
+        const reader = new FileReader();
+        reader.onloadend = (finishedEvent) => {
+            const {
+                currentTarget : { result },
+            } = finishedEvent;
+            setAttachment(result);
+        };
+        reader.readAsDataURL(theFile);
+    };
+    const onClearAttachment = () => setAttachment(null)
     return (
     <div>
         <form onSubmit={onSubmit}>
@@ -39,8 +59,14 @@ const Home = ({ userObj }) => {
                 placeholder="What's on your mind?" 
                 maxLength={120} 
             />
-            <input type="file" accept="image/*" />
+            <input type="file" accept="image/*" onChange={onFileChange}/>
             <input type="submit" value="Jweet" />
+            {attachment && (
+                <div>
+                    <img src = {attachment} width = "50px" height="50px" />
+                    <button onClick={onClearAttachment}>Clear</button>
+                </div>
+            )}
         </form>
         <div>
             {jweets.map((jweet) => (
